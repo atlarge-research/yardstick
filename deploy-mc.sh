daac26ab4307eeba4fa05703c9301cde8b7f97b1
Jesse Donkervliet <jdonkerv@fs3.cm.cluster>
2016-12-18 15:26:41 +0100

    Debugging.

--- deploy-mc.sh
+++ deploy-mc.sh
@@ -3,20 +3,26 @@
 RUNSERVERCOMMAND="nohup ./runserver.sh > server.log 2>&1 &"
 
 # Reserve a number of nodes.
-NODES=$(./reserve-nodes.sh $1)
+module load prun
+preserve -np $! -t 15:00
+sleep 2
+NODES=$(preserve -long-list | grep $(whoami) | cut -d'	' -f9)
 # Appoint master. This will run MC Server.
 MASTER=$(echo $NODES | cut -d' ' -f1)
 # Appoint slaves. This will run MC clients.
 SLAVES=$(echo $NODES | cut -d' ' -f2-)
 
 # Start the MC Server.
+echo "Starting server on $MASTER"
 ssh $MASTER $RUNSERVERCOMMAND
 # Start the MC Clients.
 for NODE in $SLAVES; do
+	echo "Starting client on $NODE"
 	ssh $NODE "nohup ./runclient.sh $MASTER > slave-$NODE.log 2>&1 &"
 done
 
 # Sleep for experiment duration.
+echo "Experiment is running. Waiting $2 seconds."
 sleep $2
 
 # Terminate all processes.
--- reserve-nodes.sh
+++ reserve-nodes.sh
@@ -3,5 +3,6 @@
 module load prun
 
 preserve -np $1 -t 15:00
+sleep 2
 NODES=$(preserve -long-list | grep $(whoami) | cut -d'	' -f9)
 echo $NODES
