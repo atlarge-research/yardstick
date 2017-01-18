87ae8fbe941115539a1a8566322657c1edfc2085
Jesse Donkervliet <jesse@jdonkervliet.com>
2017-01-18 16:57:29 +0100

    Started experiment run script for prun.

---
+++ run-experiment.py
@@ -0,0 +1,24 @@
+
+import sys
+import os
+import socket
+
+def main(args):
+    hostnames = sorted(os.environ['PRUN_PE_HOSTS'].split())
+    thismachine = socket.gethostname()
+    thisindex = hostnames.index(thismachine)
+
+    thisrole = "UNKNOWN"
+    with open('roles', 'r') as fin:
+        total = 0
+        for line in fin:
+            num, role = line.split()
+            num = int(num)
+            num += total
+            if thisindex < num:
+               thisrole = role 
+               break
+    print thisrole
+
+if __name__ == "__main__":
+    main(sys.args[1:])
