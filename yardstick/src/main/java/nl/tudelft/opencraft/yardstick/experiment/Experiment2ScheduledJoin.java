dec16222d96b189c7bb68315193c1abb95d1b3d9
Jesse Donkervliet <j.j.r.donkervliet@vu.nl>
2020-10-12 12:52:46 +0200

    Fix incorrent isDone calculation in experiment 2

--- yardstick/src/main/java/nl/tudelft/opencraft/yardstick/experiment/Experiment2ScheduledJoin.java
+++ yardstick/src/main/java/nl/tudelft/opencraft/yardstick/experiment/Experiment2ScheduledJoin.java
@@ -1,8 +1,9 @@
 package nl.tudelft.opencraft.yardstick.experiment;
 
+import com.github.steveice10.packetlib.Client;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-import com.github.steveice10.packetlib.Client;
 
 public class Experiment2ScheduledJoin extends Experiment {
 
@@ -24,6 +25,8 @@ public class Experiment2ScheduledJoin extends Experiment {
     // All connections
     private final List<Client> clients = new ArrayList<>();
 
+    private Instant endTime;
+
     public Experiment2ScheduledJoin() {
         super(2, "Gradually lets bots join a server in a scheduled manner. Supports a clustered approach.");
     }
@@ -44,7 +47,7 @@ public class Experiment2ScheduledJoin extends Experiment {
             return true;
         }
 
-        return tick > botsTotal * interval + 10_000;
+        return endTime != null && Instant.now().isAfter(endTime);
     }
 
     @Override
@@ -79,6 +82,7 @@ public class Experiment2ScheduledJoin extends Experiment {
 
         if (step == botsTotal) {
             logger.info("All bots have joined. Sleeping 10 seconds");
+            endTime = Instant.now().plusSeconds(10);
         }
     }
 
