775375d42bb6bf23847a6770594c892291eecac4
Jesse Donkervliet <j.j.r.donkervliet@vu.nl>
2020-12-14 22:27:25 +0100

    Add license to Java source files

--- yardstick/src/main/java/nl/tudelft/opencraft/yardstick/experiment/Experiment6InteractWalk.java
+++ yardstick/src/main/java/nl/tudelft/opencraft/yardstick/experiment/Experiment6InteractWalk.java
@@ -1,3 +1,21 @@
+/*
+ * Yardstick: A Benchmark for Minecraft-like Services
+ * Copyright (C) 2020 AtLarge Research
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package nl.tudelft.opencraft.yardstick.experiment;
 
 import nl.tudelft.opencraft.yardstick.model.MoveInteractModel;
