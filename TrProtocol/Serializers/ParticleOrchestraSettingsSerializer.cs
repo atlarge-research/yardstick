370fe17dc12e71de20e1c02ae948f261779fb818
Megghy <megghy@qq.com>
2021-10-12 10:38:05 +0800

    fix particles module and playerhurt

---
+++ TrProtocol/Serializers/ParticleOrchestraSettingsSerializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrProtocol.Models;
+
+namespace TrProtocol.Models
+{
+    [Serializer(typeof(ParticleOrchestraSettingsSerializer))]
+    public partial class ParticleOrchestraSettings
+    {
+		private class ParticleOrchestraSettingsSerializer : FieldSerializer<ParticleOrchestraSettings>
+		{
+			protected override ParticleOrchestraSettings _Read(BinaryReader br)
+			{
+				return DeserializeFrom(br);
+			}
+
+            protected override void _Write(BinaryWriter bw, ParticleOrchestraSettings t)
+            {
+                t.Serialize(bw);
+            }
+        }
+	}
+}
