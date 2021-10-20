e38ea1d36e80a4bdd2e2d87786391b31fdc0cf6d
Megghy <megghy@qq.com>
2021-10-20 18:02:15 +0800

    add TileSquare packet, fix liquid update

---
+++ TrProtocol/Models/TileChangeType.cs
@@ -0,0 +1,13 @@
+﻿using TrProtocol.Serializers;
+
+namespace TrProtocol.Models
+{
+    [Serializer(typeof(ByteEnumSerializer<TileChangeType>))]
+    public enum TileChangeType : byte
+    {
+        None,
+        LavaWater,
+        HoneyWater,
+        HoneyLava
+    }
+}
