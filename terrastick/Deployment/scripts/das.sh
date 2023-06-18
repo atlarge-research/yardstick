ef7444324e988bcf0c2e509721f3fa6d9c9b9d09
Anmol Bhatia <anmolbhatia05@gmail.com>
2023-06-18 15:52:36 +0200

    more changes

--- terrastick/Deployment/scripts/das.sh
+++ terrastick/Deployment/scripts/das.sh
@@ -121,7 +121,7 @@ remote_commands=$(cat <<CMD
     unzip terrastick.zip && rm terrastick.zip
     mv yardstick-$TERRASTICK_VERSION/terrastick/Deployment/worlds ../server/
     mv yardstick-$TERRASTICK_VERSION/terrastick/Deployment/metrics-configs/prometheus-terrastick.yml ../prometheus/prometheus-2.37.8.linux-amd64/
-    mv yardstick-$TERRASTICK_VERSION/terrastick/Deployment/metrics-configs/server-process-exporter.yml ../server/process-exporter-0.7.10.linux-amd64/
+    mv yardstick-$TERRASTICK_VERSION/terrastick/Deployment/metrics-configs/server-process-exporter.yaml ../server/process-exporter-0.7.10.linux-amd64/
     cd yardstick-$TERRASTICK_VERSION/terrastick/PlayerEmulations/TrClientTest && dotnet build -r linux-x64 -c Release --no-self-contained || echo "Build failed"
     module load prun
     preserve -llist
@@ -152,10 +152,11 @@ remote_commands=$(cat <<CMD
     ssh \$server_node 'cd ~/$DIR_NAME/server && screen -S server -d -m bash -c "./TShock.Server -world ~/$DIR_NAME/server/worlds/$WORLD_NAME.wld"' && echo "Server started on \$server_node"
     echo "waiting for server to start" && sleep 10
 
-    # # start process exporter on server node
-    # ssh \$server_node 'cd ~/$DIR_NAME/server && screen -S process-exporter -d -m bash -c "./process-exporter -config.path server-process-exporter.yaml"' && echo "Process exporter started on \$server_node"
-    # # start prometheus on prometheus node
-    # ssh \$prometheus_node 'cd ~/$DIR_NAME/server && screen -S prometheus -d -m bash -c "prometheus --config.file=prometheus.yml"' && echo "Prometheus started on \$prometheus_node"
+    # start process exporter on server node
+    ssh \$server_node 'cd ~/$DIR_NAME/server/process-exporter-0.7.10.linux-amd64 && screen -S process-exporter -d -m bash -c "./process-exporter -config.path server-process-exporter.yaml"' && echo "Process exporter started on \$server_node"
+    
+    # start prometheus on prometheus node
+    ssh \$prometheus_node 'cd ~/$DIR_NAME/prometheus/prometheus-2.37.8.linux-amd64 && screen -S prometheus -d -m bash -c "prometheus --config.file=prometheus-terrastick.yml"' && echo "Prometheus started on \$prometheus_node"
     # echo "waiting for prometheus to start" && sleep 10
     
     for node in \$bot_nodes; do
