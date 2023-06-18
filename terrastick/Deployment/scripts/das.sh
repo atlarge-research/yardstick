7e0db70b3a203755b452bd14023046cede935c30
Anmol Bhatia <anmolbhatia05@gmail.com>
2023-06-18 14:39:37 +0200

    new changes

--- terrastick/Deployment/scripts/das.sh
+++ terrastick/Deployment/scripts/das.sh
@@ -102,28 +102,31 @@ remote_commands=$(cat <<CMD
     dir_name="$DIR_NAME"
     mkdir -p "\$dir_name"
     cd "\$dir_name"
-    mkdir -p server bot
-    cd server
+    mkdir -p server bot prometheus
+    cd prometheus
+    curl -sL  https://github.com/prometheus/prometheus/releases/download/v2.37.8/prometheus-2.37.8.linux-amd64.tar.gz -o prometheus.gz
+    tar -xvf prometheus.gz && rm prometheus.gz
+    cd ../server
     curl -sL https://github.com/Pryaxis/TShock/releases/download/v5.1.3/TShock-5.1.3-for-Terraria-1.4.4.9-linux-x64-Release.zip -o TShock-5.1.3-for-Terraria-1.4.4.9-linux-x64-Release.zip
     unzip TShock-5.1.3-for-Terraria-1.4.4.9-linux-x64-Release.zip
     tar -xvf TShock-Beta-linux-x64-Release.tar
     rm TShock-Beta-linux-x64-Release.tar TShock-5.1.3-for-Terraria-1.4.4.9-linux-x64-Release.zip
+    curl -sL https://github.com/ncabatoff/process-exporter/releases/download/v0.7.10/process-exporter-0.7.10.linux-amd64.tar.gz -o process-exporter.gz
+    tar -xvf process-exporter.gz && rm process-exporter.gz
     cd ServerPlugins
     curl -sL https://github.com/atlarge-research/yardstick/releases/download/$TERRASTICK_VERSION/server-side-packet-monitor.zip -o server-side-packet-monitor.zip
     unzip -n server-side-packet-monitor.zip && rm server-side-packet-monitor.zip
     cd ../../bot
     curl -sL https://github.com/atlarge-research/yardstick/archive/refs/tags/$TERRASTICK_VERSION.zip -o terrastick.zip
     unzip terrastick.zip && rm terrastick.zip
-    curl -sL  https://github.com/prometheus/prometheus/releases/download/v2.37.8/prometheus-2.37.8.linux-amd64.tar.gz -o prometheus.gz
-    tar -xvf prometheus.gz
-    curl -sL https://github.com/ncabatoff/process-exporter/releases/download/v0.7.10/process-exporter-0.7.10.linux-amd64.tar.gz -o process-exporter.gz
-    tar -xvf process-exporter.gz
     mv yardstick-$TERRASTICK_VERSION/terrastick/Deployment/worlds ../server/
+    mv yardstick-$TERRASTICK_VERSION/terrastick/Deployment/metrics-configs/prometheus-terrastick.yml ../prometheus/prometheus-2.37.8.linux-amd64/
+    mv yardstick-$TERRASTICK_VERSION/terrastick/Deployment/metrics-configs/server-process-exporter.yml ../server/process-exporter-0.7.10.linux-amd64/
     cd yardstick-$TERRASTICK_VERSION/terrastick/PlayerEmulations/TrClientTest && dotnet build -r linux-x64 -c Release --no-self-contained || echo "Build failed"
     module load prun
     preserve -llist
-    echo "Reserving $NUM_NODES nodes for $RESERVE_DURATION"
-    preserve -np $((NUM_NODES+1)) -t $RESERVE_DURATION > reservation.txt
+    echo "Reserving $NUM_NODES nodes for $RESERVE_DURATION. Out of the nodes reserved, one will be used as the server node, one as the prometheus node, and the rest as bot nodes."
+    preserve -np $NUM_NODES -t $RESERVE_DURATION > reservation.txt
     cat reservation.txt | grep -o -E '[0-9]+' | head -n 1 > reservation_id.txt
     reservation_id=\$(cat reservation_id.txt) && rm reservation*.txt
     while ! preserve -llist | grep \$reservation_id | awk '{print \$7}' | grep -q "R" ;
@@ -140,18 +143,20 @@ remote_commands=$(cat <<CMD
     bot_nodes=\${my_array[@]:1:\${#my_array[@]}-2}
     echo "Bot nodes: \$bot_nodes"
     prometheus_node=\${my_array[-1]}
+    echo "Prometheus node: \$prometheus_node"
 
+    
     sed -i "s/export TERRASTICK_IP=.*/export TERRASTICK_IP=10.141.0.\$(echo \$server_node | sed 's/node0*\([1-9][0-9]*\)/\1/' | grep -oE '[0-9]+')/" ~/.bashrc
     sed -i 's/export TERRASTICK_WORKLOAD=.*/export TERRASTICK_WORKLOAD=TEL/' ~/.bashrc
     source ~/.bashrc
     ssh \$server_node 'cd ~/$DIR_NAME/server && screen -S server -d -m bash -c "./TShock.Server -world ~/$DIR_NAME/server/worlds/$WORLD_NAME.wld"' && echo "Server started on \$server_node"
     echo "waiting for server to start" && sleep 10
 
-    # start process exporter on server node
-    ssh \$server_node 'cd ~/$DIR_NAME/server && screen -S process-exporter -d -m bash -c "./process-exporter -config.path server-process-exporter.yaml"' && echo "Process exporter started on \$server_node"
-    # start prometheus on prometheus node
-    ssh \$prometheus_node 'cd ~/$DIR_NAME/server && screen -S prometheus -d -m bash -c "prometheus --config.file=prometheus.yml"' && echo "Prometheus started on \$prometheus_node"
-    echo "waiting for prometheus to start" && sleep 10
+    # # start process exporter on server node
+    # ssh \$server_node 'cd ~/$DIR_NAME/server && screen -S process-exporter -d -m bash -c "./process-exporter -config.path server-process-exporter.yaml"' && echo "Process exporter started on \$server_node"
+    # # start prometheus on prometheus node
+    # ssh \$prometheus_node 'cd ~/$DIR_NAME/server && screen -S prometheus -d -m bash -c "prometheus --config.file=prometheus.yml"' && echo "Prometheus started on \$prometheus_node"
+    # echo "waiting for prometheus to start" && sleep 10
     
     for node in \$bot_nodes; do
         echo "Bot node: \$node"
@@ -159,7 +164,6 @@ remote_commands=$(cat <<CMD
         echo "Bot started on \$node"
     done
     echo "Server and bots started"
-    ssh \$server_node 'screen -r server'
 CMD
 )
 
