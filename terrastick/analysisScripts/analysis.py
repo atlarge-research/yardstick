4271234054779c65dc80b3c2a1f590456e40b376
Abhilash Balaji <abhilash.balaji1998@gmail.com>
2023-07-10 18:23:57 +0200

    analysis , fix datetime parsing

--- terrastick/analysisScripts/analysis.py
+++ terrastick/analysisScripts/analysis.py
@@ -7,11 +7,10 @@ from datetime import datetime
 CURRENT_DIR = os.environ['CURRENT_DIR']
 
 # read the onle file in CURRENT_DIR/server/tshock/logs
-ServerLogs = os.listdir(CURRENT_DIR + '/server/tshock/logs')[0]
+ServerLogs = CURRENT_DIR+"/server/tshock/logs/"+os.listdir(CURRENT_DIR + '/server/tshock/logs')[0]
 
 # read the only file in CURRENT_DIR/server/tshock/PacketLogs
-PacketLogs = os.listdir(CURRENT_DIR + '/server/tshock/PacketLogs')[0]
-
+PacketLogs = CURRENT_DIR+"/server/tshock/PacketLogs/"+os.listdir(CURRENT_DIR + '/server/tshock/PacketLogs')[0]
 with open(ServerLogs, 'r') as f:
     lines = f.readlines()
 
@@ -31,42 +30,52 @@ end_time = datetime.strptime(end_time, '%Y-%m-%d %H:%M:%S')
 # get all the log lines between the start and end of the workload from PacketLogs
 with open(PacketLogs, 'r') as f:
     lines = f.readlines()
-
 # get all the lines that contain the word "GAME UPDATE TIME" between the start and end of the workload
 game_update_lines = [line for line in lines if "GAME UPDATE TIME"  in line]
 # filter all the lines between the start and end of the workload
-
-game_update_lines = [line for line in game_update_lines if start_time < datetime.strptime(line.split(' ')[0] + ' ' + line.split(' ')[1], '%Y-%m-%d %H:%M:%S') < end_time]
+l = game_update_lines[0]
+# 7/10/2023 4:05:32 PM: GAME UPDATE TIME : 600\n
+print(datetime.strptime(l.split(':')[0].strip()+':'+l.split(":")[1].strip()+":"+l.split(":")[2].strip(),"%m/%d/%Y %I:%M:%S %p"))
+game_update_lines = [
+    line for line in game_update_lines 
+    if start_time < datetime.strptime(line.split(':')[0].strip() + ':' + line.split(':')[1].strip() +":"+ line.split(':')[2].strip(), '%m/%d/%Y %I:%M:%S %p')
+                    < end_time
+]
 
 # plot the response times of the workload through time
 game_update_times = [line.split(' ')[-1] for line in game_update_lines]
 
 
-
 # get the time stamp of of the current game_update_lines
-update_time_timestamps = [datetime.strptime(line.split(' ')[0] + ' ' + line.split(' ')[1], '%Y-%m-%d %H:%M:%S') for line in game_update_lines]
+update_time_timestamps = [datetime.strptime(line.split(':')[0].strip() + ':' + line.split(':')[1].strip() +":"+ line.split(':')[2].strip(), '%m/%d/%Y %I:%M:%S %p') for line in game_update_lines]
 
 
 # plot the response times of the workload through time
-sns.lineplot(x=update_time_timestamps, y=game_update_times)
+print(len(update_time_timestamps),len(game_update_times))
+plt.plot(update_time_timestamps, game_update_times)
 # plt.title('2 * 3 Bots Heap Tiling '+'DAS5 Response Times')
-
 
 # read the experiment config from CURRENT_DIR/server/config,txt
 with open(CURRENT_DIR + '/server/config.txt', 'r') as f:
     lines = f.readlines()
 for line in lines:
     if line.__contains__('DIR_NAME'):
-        numBots = int(line.split('=')[-1])
+        numBots = line.split('=')[-1]
+        numBots = numBots.split('\n')[0]
     if line.__contains__('NUM_NODES'):
-        num_nodes = int(line.split('=')[-1])
+        num_nodes = line.split('=')[-1]
+        num_nodes = num_nodes.split('\n')[0]
     if line.__contains__('NUM_BOTS_PER_NODE'):
-        num_bots_node = int(line.split('=')[-1])
+        num_bots_node = line.split('=')[-1]
+        num_bots_node = num_bots_node.split('\n')[0]
     if line.__contains__('TERRASTICK_TILING'):
         tiling = line.split('=')[-1]
+        tiling = tiling.split("\n")[0]
+        if tiling =='':
+            tiling = "TEST"
 
 
 plt.title(str(num_nodes) + ' * ' + str(num_bots_node) + ' Bots ' + tiling + ' DAS5 Response Times')
-plt.savefig(CURRENT_DIR + '/analysisScripts/'+num_nodes+'_'+num_bots_node+'_'+tiling+'_Response_Times.pdf')
+plt.savefig(CURRENT_DIR + '/bot/yardstick-terrastick-test-v0.15/terrastick/analysisScripts/'+num_nodes+'_'+num_bots_node+'_'+tiling+'_Response_Times.pdf')
 
 
