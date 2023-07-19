e658fec54722c9b7d06dd887b46fc3ed19825f6f
Abhilash Balaji <abhilash.balaji1998@gmail.com>
2023-07-19 14:26:48 +0200

    getting duration of experiement in graph

--- terrastick/analysisScripts/analysis.py
+++ terrastick/analysisScripts/analysis.py
@@ -60,7 +60,7 @@ update_time_timestamps = [datetime.strptime(line.split(':')[0].strip() + ':' + l
 
 print(len(update_time_timestamps),len(game_update_times))
 plt.figure(figsize=(20,10))
-game_update_times = [float(time) - float(game_update_times[0]) for time in game_update_times]
+game_update_times = [float(time - game_update_times[0]) for time in game_update_times]
 plt.plot(update_time_timestamps, game_update_times)
 plt.xlabel('Time')
 plt.ylabel('Response Time (ms)')
