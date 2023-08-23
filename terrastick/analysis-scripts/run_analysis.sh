d36fda0b853e28643834ef974ebd84eb18226fff
Anmol Bhatia <anmolbhatia05@gmail.com>
2023-08-23 20:53:28 +0200

    putting logs for trouble shooting

--- terrastick/analysis-scripts/run_analysis.sh
+++ terrastick/analysis-scripts/run_analysis.sh
@@ -5,6 +5,9 @@ module load python/3.6.0
 python3.6 -m venv .env
 source .env/bin/activate
 pip install -r requirements.txt
+echo "getting exp times"
 python3.6 get_exp_times.py
+echo "getting prometheus logs"
 python3.6 get_prometheus_logs.py
+echo "getting node logs"
 python3.6 analysis.py
