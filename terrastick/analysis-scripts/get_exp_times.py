17a8c288d5aaf4179e80b6e962fa3f217aa460d3
Anmol Bhatia <anmolbhatia05@gmail.com>
2023-08-23 21:15:07 +0200

    fixing issues

--- terrastick/analysis-scripts/get_exp_times.py
+++ terrastick/analysis-scripts/get_exp_times.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import os
+import requests
 
 EXP_DIR = os.environ['DIR_NAME']
 
@@ -9,14 +10,18 @@ server_logs = EXP_DIR+"/server/tshock/logs/"+os.listdir(EXP_DIR + '/server/tshoc
 with open(server_logs, 'r') as f:
     lines = f.readlines()
 
+start_time = None
+end_time = None
 for line in lines:
     if "Starting player work load" in line:
         start_time = datetime.strptime(' '.join(line.split(' ')[:2]), '%Y-%m-%d %H:%M:%S')
         break
     
+for line in lines:
     if "WORKLOAD COMPLETE" in line:
         end_time = datetime.strptime(' '.join(line.split(' ')[:2]), '%Y-%m-%d %H:%M:%S')
         break
+
 
 with open(EXP_DIR + '/exp_times.txt', 'w') as f:
     f.write('START={}\n'.format(start_time))
