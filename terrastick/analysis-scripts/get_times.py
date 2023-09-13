e779aac3aa618d1b9abece69198cb73be5dc6202
Anmol Bhatia <anmolbhatia05@gmail.com>
2023-09-13 21:13:43 +0200

    fixing json time parsing issues

--- terrastick/analysis-scripts/get_times.py
+++ terrastick/analysis-scripts/get_times.py
@@ -10,34 +10,37 @@ server_logs = EXP_DIR+"/server/tshock/logs/"+os.listdir(EXP_DIR + '/server/tshoc
 with open(server_logs, 'r') as f:
     lines = f.readlines()
 
-def convert_to_datetime(time_str):
-    return datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S')
-
 def time_to_duration(start, end):
     delta = end - start
     return delta.total_seconds()
 
-# Initial time parsing
 bot_times = {}
 
 for line in lines:
     if "Starting player work load" in line:
-        first_bot_start_time = convert_to_datetime(' '.join(line.split(' ')[:2]))
+        first_bot_start_time = datetime.strptime(' '.join(line.split(' ')[:2]), '%Y-%m-%d %H:%M:%S')
+        first_bot_start_time = first_bot_start_time.isoformat("T") + "Z"
         break
-
+    
 for line in lines:
     if "WORKLOAD COMPLETE" in line:
-        first_bot_end_time = convert_to_datetime(' '.join(line.split(' ')[:2]))
+        first_bot_end_time = datetime.strptime(' '.join(line.split(' ')[:2]), '%Y-%m-%d %H:%M:%S')
+        first_bot_end_time = first_bot_end_time.isoformat("T") + "Z"
         break
 
 first_bot_join_time = None
 for line in lines:
     if "has joined." in line:
         bot_name = re.search(r"Broadcast: (BOT_\w+)", line).group(1)
-        bot_time = convert_to_datetime(' '.join(line.split(' ')[:2]))
+        bot_time = datetime.strptime(' '.join(line.split(' ')[:2]), '%Y-%m-%d %H:%M:%S')
+        bot_time = bot_time.isoformat("T") + "Z"
         if first_bot_join_time is None:
             first_bot_join_time = bot_time
         bot_times[bot_name] = bot_time
+
+# events throught the experiment
+# first bot joins, first bot starts workloads, other bots joined, first bot ends workloads, ...
+# we are interested from first bot joins to first bot ends workloads
 
 # Convert to durations
 start_workload_duration = time_to_duration(first_bot_join_time, first_bot_start_time)
